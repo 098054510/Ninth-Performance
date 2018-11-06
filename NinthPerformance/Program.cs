41ca279e8c8291867f5e06184774c0569fc0e3f9
Eduardo de Souza <eduardohsfreitas86@gmail.com>
2018-11-06 08:00:59 -0300

    Ninth Performace/Program made in C#

---
+++ NinthPerformance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace NinthPerformance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, sqr;
+            x = int.Parse(Console.ReadLine());
+            sqr = x * x;
+            Console.WriteLine(sqr);
+            while(x != 0)
+            {
+                x = int.Parse(Console.ReadLine());
+                sqr = x * x;
+                Console.WriteLine(sqr);
+            }
+            Console.ReadLine();
+        }
+    }
+}
